0828414f77bb00ec74e6cbbf6b3576868db924a1
Marcusvh <mvh1910@yahoo.dk>
2025-03-05 14:28:33 +0100

    added GamePieceService with different moves

---
+++ SoftLudo/SoftLudoAPI/Services/GamePieceService.cs
@@ -0,0 +1,28 @@
+﻿using LudoModels;
+
+namespace SoftLudoAPI.Services
+{
+    public class GamePieceService
+    {
+        public string MoveGamePiece(GamePiece gp, int newPosition)
+        {
+            return "value";
+        }
+        public string MoveGamePieceToGoal(GamePiece gp)
+        {
+            return "value";
+        }
+        public string MoveGamePieceToHome(GamePiece gp)
+        {
+            return "value";
+        }
+        public string MoveGamePieceFromHomeToStart(GamePiece gp)
+        {
+            return "value";
+        }
+        public string MoveGamePieceToGoalTrack(GamePiece gp) // ehh, unsure 
+        {
+            return "value";
+        }
+    }
+}
