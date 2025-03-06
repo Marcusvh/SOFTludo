d6dfbbc978e7e73b0b765f21949353f57ca6e408
Jonas Grubbe <grubbe.p@hotmail.com>
2025-03-06 09:32:15 +0100

    Id til gamepiece

--- SoftLudo/LudoModels/GamePiece.cs
+++ SoftLudo/LudoModels/GamePiece.cs
@@ -8,6 +8,7 @@ namespace LudoModels
 {
     public class GamePiece
     {
+        public int Id { get; set; }
         public int Position { get; set; }
         public bool IsHome { get; set; }
         public bool IsGoal { get; set; }
