0ba96b5406f63cdcdbbf7adb6aa358ce95c89b50
= <valentinleonchristensen@gmail.com>
2025-04-07 14:18:17 +0200

    Clean slate

--- SoftLudo/SoftLudoAPI/Program.cs
+++ SoftLudo/SoftLudoAPI/Program.cs
@@ -1,19 +1,11 @@
-// Morten kommer til at give os teoratisk benspen (brættet går i en cirkel, kommer 7 spillere, osv)
-// måske have en liste af forskellige felter. 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
