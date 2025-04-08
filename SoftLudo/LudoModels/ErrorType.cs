fc1f67705a37285ad72c736b78555092adf23d7d
valentin christensen <valentinleonchristensen@gmail.com>
2025-04-08 14:16:34 +0200

    Add result pattern model

---
+++ SoftLudo/LudoModels/ErrorType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LudoModels;
+public enum ErrorType
+{
+    None = 0,
+    Error = 1,
+        
+        
+        
+
+}
---
+++ SoftLudo/LudoModels/Result.cs
@@ -0,0 +1,15 @@
+﻿namespace LudoModels;
+public class Result<T>
+{
+    public T? Value { get; set; }
+    public bool Success { get; set; } = false;
+    public ErrorType ErrorType { get; set; }
+
+
+    public Result() { }
+    public Result(T value)
+    {
+        Success = true;
+        Value = value; 
+    }
+}
