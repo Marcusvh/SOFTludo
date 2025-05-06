b2db1744813e4b1dc9b43e9c8603619b19be0de4
UsernameIsNotTilstede <marius.tranberg@gmail.com>
2025-05-06 15:23:55 +0200

    RollForStart and RollDice first iteration.

--- SoftLudo/LudoModels/Player.cs
+++ SoftLudo/LudoModels/Player.cs
@@ -3,5 +3,7 @@
 public class Player
 {
     public int Id { get; set; }
-    public string Name { get; set; } = null!;
+    public string Name { get; set; } = string.Empty;
+    public int? StartRoll { get; set; }
 }
+
